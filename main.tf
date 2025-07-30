f2d8702e9d01e87509f2521af1daaef438a45d9b
Aleksejs Sitdikovs <alex.sitdikovs@gmail.com>
2025-07-30 15:36:56 +0200

    First Terraform project with AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "asit-terraform-demo-bucket"
+  tags = {
+    Name        = "DemoBucket"
+    Environment = "Dev"
+  }
+}
